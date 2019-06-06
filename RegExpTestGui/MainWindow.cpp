8bf6b97c66cdee5440f3a464bc0fe6616294b440
David Calvin <davidc@dkc.(none)>
2019-06-06 06:38:27 -0700

    -changed messages a bit -colored the text

--- RegExpTestGui/MainWindow.cpp
+++ RegExpTestGui/MainWindow.cpp
@@ -23,15 +23,17 @@ void MainWindow::loadRegExp()
 
     if(regex_pattern.isEmpty())
     {
-        QStringList message_list("Failed");
-        message_list << QString("RegExp field is empty.\nNothing to do.");
-        ui->result_text_browser_->setText(message_list.join("\n"));
+        QStringList message_list("<font color=\"red\">Failed</font>");
+        message_list << QString("RegExp field is empty.");
+        message_list << QString("Nothing to do.");
+        ui->result_text_browser_->setHtml(message_list.join("<br>"));
     }
     else if(check_text.isEmpty())
     {
-        QStringList message_list("Failed");
-        message_list << QString("CheckText field is empty.\nNothing to do.");
-        ui->result_text_browser_->setText(message_list.join("\n"));
+        QStringList message_list("<font color=\"red\">Failed</font>");
+        message_list << QString("CheckText field is empty.");
+        message_list << QString("Nothing to do.");
+        ui->result_text_browser_->setHtml(message_list.join("<br>"));
     }
     else
     {
@@ -41,23 +43,38 @@ void MainWindow::loadRegExp()
 
         if(pos == -1)
         {
-            QStringList message_list("Failed");
+            QStringList message_list("<font color=\"red\">Failed</font>");
             message_list << QString("RegExp does not match CheckText.");
-            ui->result_text_browser_->setText(message_list.join("\n"));
+            ui->result_text_browser_->setHtml(message_list.join("<br>"));
         }
         else
         {
+            QStringList full_text_matches(regex.capturedTexts().value(0));
             QStringList captures = regex.capturedTexts().mid(1);
             pos += regex.capturedTexts().value(0).count();
             while((pos = regex.indexIn(check_text, pos)) != -1)
             {
+                full_text_matches << regex.capturedTexts().value(0);
                 captures << regex.capturedTexts().mid(1);
                 pos += regex.capturedTexts().value(0).count();
             }
 
 
-            QStringList message_list("Success");
+            QStringList message_list("<font color=\"green\">Success</font>");
             message_list << "RegExp matches Check Text";
+
+            const int full_text_count = full_text_matches.count();
+            if(full_text_count == 1)
+            {
+                message_list << QString("There is 1 full text match.");
+            }
+            else
+            {
+                message_list << QString("There are %1 full text matches").arg(full_text_count);
+            }
+            message_list << "\"" + full_text_matches.join("\"\n\"") + "\"";
+            message_list << "\n";
+
             const int capture_count = captures.count();
             if(capture_count == 0)
             {
@@ -75,8 +92,7 @@ void MainWindow::loadRegExp()
                 }
                 message_list << "\"" + captures.join("\"\n\"") + "\"";
             }
-
-            ui->result_text_browser_->setText(message_list.join("\n"));
+            ui->result_text_browser_->setHtml(message_list.join("<br>"));
         }
     }
 }
